217d7c02f85eba3d294d4530cc66c3ade0f054b2
migt <tavaresmiguel11@gmail.com>
2025-07-30 00:33:07 +0200

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/com/example/textapi/service/TextServiceGeneratedAiTests.java
@@ -0,0 +1,8 @@
+package com.example.textapi.service;
+
+}
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
+import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
